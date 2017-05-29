ea448df5732c6461a8fe274d3232455bff992d8d
ManojDatt <manojdatt1it@gmail.com>
2017-05-29 10:32:06 +0530

    setting defined

---
+++ addon.py
@@ -0,0 +1,10 @@
+
+import urllib, urllib2, socket, hashlib, time
+import xbmc, xbmcgui, xbmcaddon
+
+ADDON        = xbmcaddon.Addon()
+ADDONID      = ADDON.getAddonInfo('id')
+ADDONVERSION = ADDON.getAddonInfo('version')
+LANGUAGE     = ADDON.getLocalizedString
+
+socket.setdefaulttimeout(10)
